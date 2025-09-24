06c04d81b88ffc36dc91d005d371d030e43e0ad6
Michael <mikebgoldman95@gmail.com>
2025-09-24 15:33:14 +0200

    added testcontainer to integration test

--- backend/src/test/java/com/example/todoapp/TodoAppApplicationTests.java
+++ backend/src/test/java/com/example/todoapp/TodoAppApplicationTests.java
@@ -2,9 +2,23 @@ package com.example.todoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TodoAppApplicationTests {
+
+    @Container
+    static MongoDBContainer mongo = new MongoDBContainer("mongo:7");
+
+    @DynamicPropertySource
+    static void mongoProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", () -> mongo.getConnectionString());
+    }
 
     @Test
     void contextLoads() {
