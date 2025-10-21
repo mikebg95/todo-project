156e1275c7431d73b1c2c9f4e5e8774838897b3a
Michael <mikebgoldman95@gmail.com>
2025-10-21 10:56:15 +0200

    put navbar in separate component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
 
-import HomepageView from "@/views/TodoView.vue";
+import NavbarComponent from "@/components/NavbarComponent.vue";
 </script>
 
 <template>
   <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/info">Info</router-link> |
-      <router-link to="/dashboard">Dashboard</router-link> |
-      <router-link to="/todo">To-do</router-link>
-    </nav>
+    <NavbarComponent />
 
     <router-view />
   </div>
--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/info">Info</router-link> |
+    <router-link to="/dashboard">Dashboard</router-link> |
+    <router-link to="/todo">To-do</router-link>
+  </nav>
 </template>
 
 <style scoped>
