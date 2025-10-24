5418dd196786dcbccb9336707090aa4e6c746fa2
Michael <mikebgoldman95@gmail.com>
2025-10-24 10:03:53 +0200

    removed lombok; updated java to version 25 etc.

--- backend/src/main/java/com/example/todoapp/aop/ExecutionTimeAspect.java
+++ backend/src/main/java/com/example/todoapp/aop/ExecutionTimeAspect.java
@@ -1,17 +1,19 @@
 package com.example.todoapp.aop;
 
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Aspect
 @Component
 public class ExecutionTimeAspect {
 
-    @Around("@within(com.example.todoapp.aop.LogExecutionTime) ||@annotation(com.example.todoapp.aop.LogExecutionTime)")
+    private static final Logger log = LoggerFactory.getLogger(ExecutionTimeAspect.class);
+
+    @Around("@within(com.example.todoapp.aop.LogExecutionTime) || @annotation(com.example.todoapp.aop.LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.nanoTime();
         Object result = joinPoint.proceed(); // run the method
