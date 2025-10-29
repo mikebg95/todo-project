26f09786a814e335aeb9c7c9e96180b95dbf67c1
Michael <mikebgoldman95@gmail.com>
2025-10-29 14:45:07 +0100

    fix admin items page on InformationView.vue

--- frontend/src/views/InformationView.vue
+++ frontend/src/views/InformationView.vue
@@ -1,22 +1,47 @@
 <script setup>
+import {onMounted, ref} from "vue";
 import ItemService from "@/service/ItemService.js";
+
+const items = ref([]); // reactive list of items
+const error = ref('')
+
 
 async function getAllItems() {
   try {
-    await ItemService.getAllItems()
+    items.value = await ItemService.getAllItems();
+    console.log(items.value)
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
+async function deleteItem(id) {
+  try {
+    await ItemService.deleteItem(id)
+    await getAllItems()
+    // items.value = items.value.filter(i => i.id !== id)
+  } catch (e) {
+    error.value = e.message || 'Failed to delete'
+  }
+}
+
+onMounted(getAllItems);
 </script>
 
 <template>
   <div class="information">
     <h1>Information</h1>
 
-    <button @click="getAllItems">GET ALL ITEMS</button>
+    <!-- render list -->
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <span>{{ item.text }}</span>
+        <span><button @click="deleteItem(item.id)">DELETE</button></span>
+      </li>
+    </ul>
   </div>
+
+  <p v-if="error" class="error">{{ error }}</p>
 </template>
 
 <style scoped>
