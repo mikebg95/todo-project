29ef0690e2bf6912e93a4cb15cb6e17dd150c642
Michael <mikebgoldman95@gmail.com>
2025-10-31 16:34:36 +0100

    make username unique as well

--- backend/src/main/java/com/example/todoapp/model/User.java
+++ backend/src/main/java/com/example/todoapp/model/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String keycloakId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
